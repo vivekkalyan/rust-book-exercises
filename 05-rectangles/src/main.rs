9a06d8b87cbfab4ac5855616da8a1aa1ae5c6c4e
Vivek Kalyan <hello@vivekkalyan.com>
2022-07-29 22:18:07 +0800

    Add 05-rectangles

---
+++ 05-rectangles/src/main.rs
@@ -0,0 +1,59 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(scale * 30),
+        height: 50,
+    };
+    dbg!(&rect1);
+    println!("rect1 is {:?}", rect1);
+    if rect1.width() {
+        println!("Rectangle has nonzero width; it is {}", rect1.width)
+    }
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 60,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let rect4 = Rectangle::square(20);
+    println!("rect4 is {:?}", rect4);
+}
+
