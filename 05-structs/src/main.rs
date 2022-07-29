c8ed339555b968f67d752083c01be0004aecafd7
Vivek Kalyan <hello@vivekkalyan.com>
2022-07-29 21:53:21 +0800

    Add 05-structs

---
+++ 05-structs/src/main.rs
@@ -0,0 +1,32 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+fn main() {
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("someoneelse@example.com");
+
+    let user2 = build_user(String::from("new@example.com"), String::from("newuser"));
+
+    let user3 = User{
+        email: String::from("new1@example.com"),
+        ..user2
+    };
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
