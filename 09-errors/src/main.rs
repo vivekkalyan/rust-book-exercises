ccba0e7401294d5801d889032040a3b39877997f
Vivek Kalyan <hello@vivekkalyan.com>
2022-08-01 19:19:37 +0800

    Add 09-errors

---
+++ 09-errors/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io;
+use std::io::ErrorKind;
+use std::io::Read;
+
+fn main() {
+    error_matching();
+    error_propagating();
+    error_propagating_operator();
+}
+
+fn error_matching() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+}
+
+fn error_propagating() {
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let f = File::open("hello.txt");
+
+        let mut f = match f {
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+
+        let mut s = String::new();
+
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
+        }
+    }
+    let username = read_username_from_file();
+}
+
+fn error_propagating_operator() {
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let mut s = String::new();
+
+        File::open("hello.txt")?.read_to_string(&mut s)?;
+
+        Ok(s)
+    }
+    let username = read_username_from_file();
+}
